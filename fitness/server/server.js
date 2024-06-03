c53885938ed060bda6361d2c8482f42d91a1b246
thao0896 <thao0896@umn.edu>
2024-06-03 10:27:05 -0500

    fixing the backend

--- fitness/server/server.js
+++ fitness/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const { 
     getAllData, 
@@ -14,6 +15,12 @@ app.listen(PORT, () => {
     console.log(`Server Start on ${PORT}`);
 })
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //EXERCISE API
 app.post("/api/addExercise", async (request, response) => {
